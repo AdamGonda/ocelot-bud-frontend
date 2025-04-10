e712cc58e4102a65d1b7314421090d67436a7006
adamgonda <adamgondagyula@gmail.com>
2025-04-10 15:46:07 +0200

    Add success alert after file upload completion in FilePicker component

--- src/components/content/dashboard/filepicker.tsx
+++ src/components/content/dashboard/filepicker.tsx
@@ -46,6 +46,8 @@ const FilePicker = () => {
       }).then((response) => {
         setUploading(false);
         setSelectedFiles([]);
+
+        alert("File uploaded successfully");
       });
     }
   };
